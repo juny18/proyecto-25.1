b90f895a59790ac620dd910e8841ad06337b8567
juny18 <93839402+juny18@users.noreply.github.com>
2021-12-18 20:49:35 -0800

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,30 @@
+class Paper{
+    constructor()
+	{
+        this.image=loadImage("paper.png")     
+
+		var options={
+		isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+
+ 
+
+       }
+}
+
+display()
+	{
+
+			push()
+
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			pop()
+			
+	}
+
+
+    }
